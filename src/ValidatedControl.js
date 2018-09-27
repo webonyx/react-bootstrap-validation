0236720c4ea9fb9a2d49fd0f144e6c07b7a75ce2
jared <jared@webonyx.com>
2018-09-27 10:44:04 -0700

    Adding ValidatedControl class for compatibility with react-bootstrap 0.32.1

---
+++ src/ValidatedControl.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FormControl } from 'react-bootstrap';
+
+export default class ValidatedControl extends FormControl {
+    constructor(props) {
+        super(props);
+
+        if (!props._registerInput || !props._unregisterInput) {
+            throw new Error('Input must be placed inside the Form component');
+        }
+    }
+
+    componentWillMount() {
+        if (FormControl.prototype.componentWillMount) {
+            super.componentWillMount();
+        }
+
+        this.props._registerInput(this);
+    }
+
+    componentWillUnmount() {
+        if (FormControl.prototype.componentWillUnmount) {
+            super.componentWillUnmount();
+        }
+
+        this.props._unregisterInput(this);
+    }
+}
+
+ValidatedControl.propTypes = Object.assign({}, FormControl.propTypes, {
+    name           : React.PropTypes.string.isRequired,
+    validationEvent: React.PropTypes.oneOf([
+        '', 'onChange', 'onBlur', 'onFocus'
+    ]),
+    validate       : React.PropTypes.oneOfType([
+        React.PropTypes.func,
+        React.PropTypes.string
+    ]),
+    errorHelp      : React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.object
+    ])
+});
+
+ValidatedControl.defaultProps = Object.assign({}, FormControl.defaultProps, {
+    validationEvent: ''
+});
