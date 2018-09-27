cf5bca8a0b26ad4c672b6a1a1fb001f6adf4b1e6
jared <jared@webonyx.com>
2018-09-27 10:57:51 -0700

    Adding ValidatedControl to the index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Form from './Form';
 import ValidatedInput from './ValidatedInput';
+import ValidatedControl from './ValidatedControl';
 import Radio from './Radio';
 import RadioGroup from './RadioGroup';
 import Validator from './Validator';
 import FileValidator from './FileValidator';
 
-export { Form, ValidatedInput, Radio, RadioGroup, Validator, FileValidator };
+export {Form, ValidatedInput, ValidatedControl, Radio, RadioGroup, Validator, FileValidator};
